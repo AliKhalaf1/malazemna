0cc30615d71f65ce943bccb29b005c6748b2397b
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-07-19 22:14:15 +0300

    Initial files

---
+++ Malazemna/src/Modules/Orders/Orders.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+import "./Orders.scss";
+export class Orders extends PureComponent {
+  render() {
+    return <div>Orders</div>;
+  }
+}
+
+export default Orders;
