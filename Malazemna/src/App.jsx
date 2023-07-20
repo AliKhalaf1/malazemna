a933b085940264959899b052b79329d4670ceae8
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-07-20 21:42:38 +0300

    Revert "Merge pull request #1 from AliKhalaf1/main"
    This reverts commit 5393a42ebe938545ca397e16b5d8dc27c48a583d, reversing
    changes made to f45a2d59cfb0b8d6203be2eefbbf1066c6519785.

--- Malazemna/src/App.jsx
+++ Malazemna/src/App.jsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./Modules/Home/Home.jsx";
-import Library from "./Modules/Library/Library";
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
-};
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
 
 export default App;
