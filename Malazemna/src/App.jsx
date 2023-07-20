a162fb5d7e09076c438bc217c258af9022835eec
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-07-20 14:59:46 +0300

    first home page commit

--- Malazemna/src/App.jsx
+++ Malazemna/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Modules/Home/Home.jsx";
+import Library from "./Modules/Library/Library";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
