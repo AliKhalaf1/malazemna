dbfd032cd6ecea2e4fb68f06b58d61300bbb9f89
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-07-21 17:32:46 +0300

    navbar

--- Malazemna/src/App.jsx
+++ Malazemna/src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Modules/Home/Home.jsx";
 import Library from "./Modules/Library/Library";
-
+import NavBar from "./Modules/NavBar/NavBar.jsx";
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <NavBar />
+    </>
   );
 };
 
