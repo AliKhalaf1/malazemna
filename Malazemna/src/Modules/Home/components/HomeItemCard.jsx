1e8416605bb8d720941056d84e3d9c4ee99d6b94
h4z3m <h4z3m.private@gmail.com>
2023-07-24 00:16:26 +0300

    Added orders page for user's orders

--- Malazemna/src/Modules/Home/components/HomeItemCard.jsx
+++ Malazemna/src/Modules/Home/components/HomeItemCard.jsx
@@ -7,24 +7,26 @@ const HomeItemCard = ({ item }) => {
   console.log(item);
   return (
     <>
-      <Card>
-        <Card.Img variant="top" src={item.imageLink} />
-        <Card.Body>
-          <Card.Title>{item.name}</Card.Title>
-          <div style={{ display: "flex" }}>
-            <Rating
-              name="read-only"
-              value={item.Rating}
-              precision={0.5}
-              readOnly
-            />
-            <h1 className="ml-1 rating">({item.number_of_ratings})</h1>
-          </div>
-          <Card.Text className="location my-1">
-            -{item.location} (<b>{item.distance_from_you}km away</b>){" "}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="home">
+        <Card>
+          <Card.Img variant="top" src={item.imageLink} />
+          <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <div style={{ display: "flex" }}>
+              <Rating
+                name="read-only"
+                value={item.Rating}
+                precision={0.5}
+                readOnly
+              />
+              <h1 className="ml-1 rating">({item.number_of_ratings})</h1>
+            </div>
+            <Card.Text className="location my-1">
+              -{item.location} (<b>{item.distance_from_you}km away</b>){" "}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 };
---
+++ Malazemna/src/Modules/Orders/components/OrderCard.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Rating from "@mui/material/Rating";
+import PropTypes from "prop-types";
+import { IoIosRefresh } from "react-icons/io";
+import "./OrderCard.scss";
+import { Card, Button } from "react-bootstrap";
+const OrderCard = ({ order }) => {
+  console.log(order);
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Card.Img variant="top" src={order.imageLink} />
+          <div>
+            <Card.Title>{order.library_name}</Card.Title>
+            <Card.Subtitle
+              className={
+                `status my-1 ` +
+                (order.status === "Pending"
+                  ? "text-warning"
+                  : order.status === "Successful"
+                  ? "text-success"
+                  : "text-danger")
+              }
+            >
+              {order.status}
+            </Card.Subtitle>
+            <Card.Text className="mt-1 date">{order.order_date}</Card.Text>
+            <Card.Text className="mt-1 id">
+              {"Order ID: "}
+              {order.id}
+            </Card.Text>
+          </div>
+        </Card.Body>
+        <Card.Footer className="text-end">
+          <Button variant="outline-primary">
+            <div className="button-icon">
+              <IoIosRefresh size={14} />
+              <span> {"Re-order"}</span>
+            </div>
+          </Button>
+        </Card.Footer>
+      </Card>
+    </>
+  );
+};
+
+OrderCard.propTypes = {
+  order: PropTypes.shape({
+    library_name: PropTypes.string.isRequired,
+    order_date: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    status: PropTypes.oneOf(["Pending", "Successful", "Cancelled"]),
+    imageLink: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default OrderCard;
