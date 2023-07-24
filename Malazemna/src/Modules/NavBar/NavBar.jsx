9403e35e50dbec7604e2f1d139c35d4b534afaf9
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-07-24 08:58:19 +0300

    instagram icon

--- Malazemna/src/Modules/NavBar/NavBar.jsx
+++ Malazemna/src/Modules/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import ReceiptIcon from "@mui/icons-material/Receipt";
 import { useLocation } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 const NavBar = () => {
   const location = useLocation();
@@ -27,16 +28,10 @@ const NavBar = () => {
           </div>
         </a>
       </div>
-      <div
-        className={
-          location.pathname === "/search"
-            ? "mobile-bottom-nav__item mobile-bottom-nav__item--active"
-            : "mobile-bottom-nav__item"
-        }
-      >
+      <div>
         <div className="mobile-bottom-nav__item-content">
           <i className="material-icons">
-            <SearchIcon />
+            <SocialIcon url="https://www.instagram.com/matoob3/" />
           </i>
           Search
         </div>
