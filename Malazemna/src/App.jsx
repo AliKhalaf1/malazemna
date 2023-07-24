37d84d9e7c8abb8564e92dd0ccc0acf239bcab5a
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2023-07-24 08:08:39 +0300

    orders routing

--- Malazemna/src/App.jsx
+++ Malazemna/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/library/:id" element={<Library />} />
-        <Route path="/order" element={<Orders />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
       <NavBar />
